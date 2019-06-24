875263941e967ea0c76031607926347d30810286
shenae <shhnae@gmail.com>
2019-06-24 12:49:21 -0400

    added items to portfolio page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Switch, Route, Link } from 'react-router-dom';
 import About from './components/About/About';
 import Portfolio from './components/Portfolio/Portfolio';
 import Resume from './components/Resume/Resume';
-import image1 from "./Images/me_at_ga_portfolio.png";
+import image1 from "./components/Images/me_at_ga_portfolio.png";
 
 function App() {
   return (
@@ -12,21 +12,8 @@ function App() {
 <nav class="navbar" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
   <h1 class="title" id="title">Shenae Simmons</h1>
-      {/* <h2 id="title">front end developer</h2> */}
   </div>
 </nav>
-    {/* <section class="hero is-small">
-  <div class="hero-body">
-    <div class="container">
-      <h1 class="title">
-        Shenae Simmons
-      </h1>
-      <h2 class="subtitle">
-      front end developer
-      </h2>
-    </div>
-  </div>
-</section> */}
     <div class="tile is-ancestor">
     <div class="tile is-parent is-8">
     <div class="tile is-child box">
@@ -67,8 +54,6 @@ function App() {
   </ul>
 </nav>
     </div>
-
-    // color palette purple: #753ECE, dark grey: #4B4453 light grey: #B0A8B9, light purple: #BAA1D5
   );
 }
 
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,34 +1,108 @@
 import React, { Component } from 'react';
+import image2 from "../Images/circumvelo_screenshot.png";
+import image3 from "../Images/made2fit-screenshot.png";
+import image4 from "../Images/reciplease_screenshot.png";
 
 class Portfolio extends Component {
   render() {
     return (
 
-  <div>
-    <div class="card is-small">
-  <div class="card-content">
-    <p class="title">
-      “There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors.”
+<div>
+  <article class="media">
+  <figure class="media-left">
+    <p class="image is-128x128">
+    <img src={image2} alt="screenshot of bike sharing app with map" />
     </p>
-    <p class="subtitle">
-      Jeff Atwood
-    </p>
-  </div>
-  <footer class="card-footer">
-    <p class="card-footer-item">
-      <span>
-        View on <a href="https://twitter.com/codinghorror/status/506010907021828096">Twitter</a>
-      </span>
-    </p>
-    <p class="card-footer-item">
-      <span>
-        Share on <a href="#">Facebook</a>
-      </span>
-    </p>
-  </footer>
+  </figure>
+  <div class="media-content">
+    <div class="content">
+      <p>
+      <a href="https://circumvelo.herokuapp.com">
+        <strong id="title">CircumVelo</strong>
+         </a><small> React, RestAPI, Javascript</small> 
+        <br />
+        <p id="description">
+        The selection from the dropdown list re­renders the location on the map by integrating the longitude and latitude coordinates from both the MapBoxAPI and CityBikes API.</p>
+      </p>
+      </div>
+      <nav class="level is-mobile">
+      <div class="level-left">
+        <a class="level-item">
+        <a href="https://circumvelo.herokuapp.com">
+          <span class="icon is-small"><i class="fas fa-link" id="title"></i></span></a>
+        </a>
+        <a class="level-item">
+        <a href="https://github.com/shenae/circumvelo">
+          <span class="icon is-small"><i class="fab fa-github" id="title"></i></span></a>
+        </a>
+      </div>
+    </nav>
 </div>
-    
-  </div>
+</article>
+<article class="media">
+  <figure class="media-left">
+    <p class="image is-128x128">
+    <img src={image3} alt="screenshot of ux collaboration clothing site" />
+    </p>
+  </figure>
+  <div class="media-content">
+    <div class="content">
+      <p>
+      <a href="https://epic-jones-4619fa.netlify.com/">
+        <strong id="title">Made2fit</strong>
+         </a><small> React, HTML, CSS, Javascript</small> 
+        <br />
+        <p id="description">
+        I was the project manager for this group collaboration website with 4 UX/DI students and 3 fellow SEI students. In addition to using InDesign and Zeppelin for wireframes and mockups, we contributed to the overall concept and design.</p>
+      </p>
+      </div>
+      <nav class="level is-mobile">
+      <div class="level-left">
+        <a class="level-item">
+        <a href="https://epic-jones-4619fa.netlify.com/">
+          <span class="icon is-small"><i class="fas fa-link" id="title"></i></span></a>
+        </a>
+        <a class="level-item">
+          <a href="https://github.com/shenae/made2fit"><span class="icon is-small"><i class="fab fa-github" id="title"></i></span></a>
+        </a>
+      </div>
+    </nav>
+</div>
+</article>
+<article class="media">
+  <figure class="media-left">
+    <p class="image is-128x128">
+    <img src={image4} alt="screenshot of bike sharing app with map" />
+    </p>
+  </figure>
+  <div class="media-content">
+    <div class="content">
+      <p>
+      <a href="http://reciplease.surge.sh/">
+        <strong id="title">ReciPlease</strong>
+         </a><small> Express, PostgreSQL, OAuth, React</small> 
+        <br />
+        <p id="description">
+        As the project manager for this full stack website, I supervised the version control for the backend and front end components. I composed the ux design for the frontend components.</p>
+      </p>
+      </div>
+      <nav class="level is-mobile">
+      <div class="level-left">
+        <a class="level-item">
+        <a href="http://reciplease.surge.sh/">
+          <span class="icon is-small"><i class="fas fa-link" id="title"></i></span></a>
+        </a>
+        <a class="level-item">
+        <a href="https://github.com/shenae/reciplease">
+          <span class="icon is-small"><i class="fab fa-github" id="title"></i></span></a>
+        </a>
+      </div>
+    </nav>
+</div>
+</article>
+
+
+</div>
 
 
         );
